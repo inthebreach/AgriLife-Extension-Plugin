46a79a67bdc53cf4c52f3d6de1e545395e1f569a
Travis Ward <travis@travsiward.com>
2014-07-10 14:58:53 -0500

    Replace global styles with extension styles.

--- src/Asset.php
+++ src/Asset.php
@@ -10,6 +10,15 @@ class Asset {
 
         $this->add_image_sizes();
 
+        // Register global styles used in the theme
+        add_action( 'wp_enqueue_scripts', array( $this, 'register_ext_styles' ) );
+
+        // Enqueue extension styles
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_ext_styles' ) );
+
+        // Dequeue global styles
+        add_action( 'wp_print_styles', array( $this, 'dequeue_global_styles'), 5 );
+
 	}
 
 	public function register_js() {
@@ -39,6 +48,47 @@ class Asset {
     public function add_image_sizes() {
 
         add_image_size( 'program-solution_single', 558, 287, true );
+
+    }
+
+    /**
+     * Registers all styles used within the plugin
+     * @since 1.0
+     * @return void
+     */
+    public function register_ext_styles() {
+
+        wp_register_style(
+            'extension-styles',
+            AF_THEME_DIRURL . '/css/ext.css',
+            array(),
+            '',
+            'screen'
+        );
+
+    }
+
+    /**
+     * Enqueues extension styles
+     * @since 1.0
+     * @global $wp_styles
+     * @return void
+     */
+    public function enqueue_ext_styles() {
+
+        wp_enqueue_style( 'extension-styles' );
+
+    }
+
+    /**
+     * Dequeues global styles
+     * @since 1.0
+     * @global $wp_styles
+     * @return void
+     */
+    public function dequeue_global_styles() {
+
+        wp_dequeue_style( 'default-styles' );
 
     }
 
