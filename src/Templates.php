faa266eb3bfca4d06bcfdc8a17d9484729192c26
Travis Ward <travis@travsiward.com>
2014-09-12 11:43:54 -0500

    remove fields from blog posts

--- src/Templates.php
+++ src/Templates.php
@@ -11,6 +11,10 @@ class Templates {
         add_filter( 'genesis_single_crumb' , array( $this, 'ext_add_cpt_crumb' ), 10, 2);
         add_filter( 'genesis_archive_crumb' , array( $this, 'ext_add_cpt_crumb' ), 10, 2);
 
+        // Remove some fields from posts
+        add_filter( 'genesis_post_info', array( $this, 'ext_post_info_filter'), 10, 2);
+        add_filter( 'genesis_post_meta', array( $this, 'ext_remove_post_entry_footer'), 10, 2);
+
     }
 
 
@@ -37,8 +41,9 @@ class Templates {
 
     /**
      * Shows the programs or solutions template when needed
-     * @param  string $single_template The default single template
-     * @return string                  The correct single template
+     * @param  string $crumb         The default breadcrumb info
+     * @param  string $args          The default breadcrumb args
+     * @return string                The correct single template
      */
     function ext_add_cpt_crumb( $crumb, $args ) {
         if( is_singular( 'extension-solutions' ) ||  is_singular( 'extension-programs' ) ||  is_singular( 'post' ) )
@@ -72,5 +77,14 @@ class Templates {
         return $crumb;
     }
 
+    function ext_post_info_filter($post_info) {
+        $post_info = '[post_date]';
+        return $post_info;
+    }
+
+    function ext_remove_post_entry_footer() {
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+    }
 
 }
