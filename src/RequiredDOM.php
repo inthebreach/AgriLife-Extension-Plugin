5e036a4d735aa391e948970a5d75810d2c918696
Travis Ward <travis@travsiward.com>
2014-10-10 16:02:00 -0500

    validation fixes

--- src/RequiredDOM.php
+++ src/RequiredDOM.php
@@ -187,7 +187,8 @@ class RequiredDOM {
 
         $output = '
             <div class="footer-container-tamus">
-                <a href="http://tamus.edu/" title="Texas A&M University System"><img class="footer-tamus" src="'.AG_EXT_DIR_URL.'/img/logo-tamus.png" title="Texas A&M University System Member" alt="Texas A&M University System Member" /><noscript><img src="//agrilifecdn.tamu.edu/wp-content/themes/AgriLife-Beta/images/footer-tamus.png" title="Texas A&M University System Member" alt="Texas A&M University System Member" /></noscript></a>
+                <a href="http://tamus.edu/" title="Texas A&amp;M University System"><img class="footer-tamus" src="'.AG_EXT_DIR_URL.'/img/logo-tamus.png" title="Texas A&amp;M University System Member" alt="Texas A&amp;M University System Member" />
+                <noscript><img src="//agrilifecdn.tamu.edu/wp-content/themes/AgriLife-Beta/images/footer-tamus.png" title="Texas A&amp;M University System Member" alt="Texas A&amp;M University System Member" /></noscript></a>
             </div>';
 
         echo $output;
